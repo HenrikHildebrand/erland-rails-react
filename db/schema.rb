8d8461a8af0eb1f713deafc7766e6021f350141d
magnusbrattlof <brattlof.magnus@gmail.com>
2020-03-20 18:08:49 +0100

    fixed resource issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_142515) do
+ActiveRecord::Schema.define(version: 2020_03_20_151922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 2020_03_20_142515) do
     t.bigint "admin_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "invite_only"
+    t.boolean "public"
+    t.integer "initial_credits"
     t.index ["admin_id"], name: "index_events_on_admin_id"
   end
 
