7829fd4acef4dbcd8343e1fd7f96f222ac533c3b
magnusbrattlof <brattlof.magnus@gmail.com>
2020-03-20 16:07:23 +0100

    added CRUD for events

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,6 +1,7 @@
 class V1::BaseController < ActionController::API
-  include ActionController::MimeResponds
-  before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
-  acts_as_token_authentication_handler_for User
+    include ActionController::MimeResponds
+    # protect_from_forgery with: :exception
+    # skip_before_action :verify_authenticity_token
+    # before_action :authenticate_user!
+    acts_as_token_authentication_handler_for User
 end
