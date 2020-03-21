c28a7fac321d0d3bbcbe0adc1c83cef60a64f8f4
magnusbrattlof <brattlof.magnus@gmail.com>
2020-03-21 12:38:27 +0100

    removed all beerpackage shit

--- app/models/beer_package.rb
+++
@@ -1,2 +0,0 @@
-class BeerPackage < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_101947) do
+ActiveRecord::Schema.define(version: 2020_03_21_100845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,19 +39,6 @@ ActiveRecord::Schema.define(version: 2020_03_21_101947) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "beer_packages", force: :cascade do |t|
-    t.bigint "event_id"
-    t.bigint "sender_id"
-    t.bigint "receiver_id"
-    t.boolean "accepted"
-    t.datetime "accepted_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["event_id"], name: "index_beer_packages_on_event_id"
-    t.index ["receiver_id"], name: "index_beer_packages_on_receiver_id"
-    t.index ["sender_id"], name: "index_beer_packages_on_sender_id"
   end
 
   create_table "events", force: :cascade do |t|
