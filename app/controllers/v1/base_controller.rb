c9d2f999c319bbcb3ac3c8a3aac4e4a8dca9620c
magnusbrattlof <brattlof.magnus@gmail.com>
2020-03-25 06:43:33 +0100

    before checkout to testing token

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -2,10 +2,7 @@ class V1::BaseController < ActionController::API
     include ActionController::MimeResponds
     protect_from_forgery with: :exception
     # skip_before_action :verify_authenticity_token
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
     acts_as_token_authentication_handler_for User
-    
-    def current_user
-        @current_user ||= authenticate
-    end
+
 end 
