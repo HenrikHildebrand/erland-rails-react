4afac3a826a7ce614f6de4d6d856087a7c93e47e
Henrik Hildebrand <henrik.hildebrand.23@gmail.com>
2020-03-25 17:25:05 +0100

    added token auth again and works

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,87 @@
+class V1::UsersController < V1::BaseController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:event_id]
+      event = find_event
+      if event
+        render json: {
+            participants: event.participants,
+            collaborators: event.collaborators,
+            admin: event.admin
+        }
+      else
+        render json: {error: 'Could not find event with users', status: 400}
+      end
+    else
+      render json: all_users
+    end
+  end
+
+  def show
+    if @user
+      render json: @user
+    else
+      render json: {message: 'Unable to find user.', status: 404}
+    end
+  end
+
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      @messages = @user.errors.messages
+      render json: {error: 'Unable to create new user.', messages: @messages, status: 400}
+    end
+  end
+
+  def edit
+    if @user
+      render json: @user
+    else
+      render json: {error: 'Unable to find user.', status: 404}
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      render json: {message: 'User successfully updated.', status: 200}
+    else
+      render json: {error: 'Unable to update user.', messages: @user.errors.messages, status: 400}
+    end
+  end
+
+  def destroy
+    if @user
+      @user.destroy
+      render json: {message: 'User successfully deleted.', status: 200}
+    else
+      render json: {error: 'Unable to delete user.', status: 400}
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:id, :first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def find_event
+    Event.find_by_id(params[:event_id])
+  end
+
+  def all_users
+    User.all
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+end
