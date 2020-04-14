c46f133e627f739f65e302964cdcce8143e3ea4e
Henrik Hildebrand <henrik.hildebrand.23@gmail.com>
2020-04-14 20:32:32 +0200

    update

--- app/javascript/src/components/Quiz/Map/Map.js
+++ app/javascript/src/components/Quiz/Map/Map.js
@@ -5,19 +5,21 @@ import { geolocated } from "react-geolocated";
 const ubit = {lat: 57.6982853, lng: 11.9752105}
 const api_key = "AIzaSyD0Mknc1_dWFz7iRLF24lVFm4edmyxw3g4";
 
-const map = (props) => {
-    return(
-        <Map
-            id="map-erland"
-            google={props.google}
-            zoom={15}
-            center={ubit}
-            // onClick={(t, map, coord) => this.onNewMarkerClick(t, map, coord, this.state.addNewMarker)}
-        >
-            {props.children}
-        </Map>
-
-    );
+class MapContainer extends React.Component {
+    
+    render(){
+        return(
+            <Map
+                id="map-erland"
+                google={this.props.google}
+                zoom={15}
+                center={ubit}
+            >
+                {this.props.children}
+            </Map>
+    
+        );
+    }
 }
 
 export default geolocated({
@@ -26,4 +28,4 @@ export default geolocated({
     },
     watchPosition: true,
     userDecisionTimeout: 1000,
-})(GoogleApiWrapper( { apiKey: api_key } )(map));
+})(GoogleApiWrapper( { apiKey: api_key } )(MapContainer));
