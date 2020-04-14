0543434c12d63865471de6794b55ff65eae9d16a
Henrik Hildebrand <henrik.hildebrand.23@gmail.com>
2020-04-14 23:30:44 +0200

    update

--- app/javascript/src/components/Quiz/Map/Map.js
+++ app/javascript/src/components/Quiz/Map/Map.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react';
-import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { geolocated } from "react-geolocated";
 
 const ubit = {lat: 57.6982853, lng: 11.9752105}
 const api_key = "AIzaSyD0Mknc1_dWFz7iRLF24lVFm4edmyxw3g4";
- 
+
 class MapContainer extends React.Component {
-  render() {
-    return (
-        <div>
-            <ErrorBoundary>
-                <Map google={this.props.google.hej.hej} zoom={15} initialCenter={ubit} >
+    
+    render(){
+        return(
+            <div>
+                <Map
+                    id="map-erland"
+                    google={this.props.google}
+                    zoom={15}
+                    initialCenter={ubit}
+                >
                     {this.props.children}
                 </Map>
-            </ErrorBoundary>
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 }
- 
-export default GoogleApiWrapper({apiKey: api_key})(MapContainer)
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    watchPosition: true,
+    userDecisionTimeout: 1000,
+})(GoogleApiWrapper( { apiKey: api_key } )(MapContainer));
